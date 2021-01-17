4da4aae3b7d668549bd299dfbef2f914c27c8612
roku3 <down01134@gmail.com>
2021-01-17 11:13:13 +0900

    Initial Commit

---
+++ myClockApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  myClockApp
+//
+//  Created by Ryoma Usui on 2021/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
